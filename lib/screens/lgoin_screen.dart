208dcdb89bcdc282b573360d198fa2ac90ef1447
Awaizkazi <Owaizkazi789@gmail.com>
2023-02-20 19:07:00 +0530

    c

---
+++ lib/screens/lgoin_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          leading: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+          title: const Text(
+            'Sign In',
+            style: TextStyle(color: Colors.black),
+          ),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Welcome Back Mate !😊',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Lorem ipsum dolor sit amet\n dada dlfnafnoeahoi fuiafibdf 😊',
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              margin: EdgeInsets.all(15),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.arrow_drop_down),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  fillColor: Colors.indigo,
+                  hintText: 'youremail@email.com',
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(15),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.lock_rounded),
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  fillColor: Colors.indigo,
+                  hintText: '************',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  backgroundColor: Colors.indigo[800],
+                  minimumSize: Size.fromHeight(50),
+                ),
+                child: const Text(
+                  'Sign in',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/welcome.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'lgoin_screen.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('assets/images/welcome.png'),
+          const Text(
+            'MarketKy',
+            style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+          ),
+          const Text(
+            'Market in your pocket.Find\n               your best outfit here.',
+            style: TextStyle(fontSize: 17),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginScreen()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                // padding: EdgeInsets.symmetric(horizontal: 20),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                backgroundColor: Colors.indigo[800],
+                minimumSize: Size.fromHeight(50),
+              ),
+              child: const Text(
+                'Get Started',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
