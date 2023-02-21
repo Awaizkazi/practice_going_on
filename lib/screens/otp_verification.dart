7202e13059477ce11fb5a1ae3c1a797d7256101c
Awaizkazi <Owaizkazi789@gmail.com>
2023-02-21 17:12:59 +0530

    Verification only 2 button adding Remaining

--- lib/screens/otp_verification.dart
+++ lib/screens/otp_verification.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
 
 class otpVerification extends StatefulWidget {
   const otpVerification({super.key});
@@ -26,6 +27,7 @@ class _otpVerificationState extends State<otpVerification> {
           centerTitle: true,
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
               padding:
@@ -40,19 +42,62 @@ class _otpVerificationState extends State<otpVerification> {
               ),
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8.0, vertical: 15),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 8.0, vertical: 1.0),
                   child: Text(
                     'OTP Code sent to your email',
                     style: TextStyle(
                       color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
                     ),
                   ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 8.0, vertical: 1.0),
+                  child: Text(
+                    'youremail@email.com',
+                    style: TextStyle(
+                      color: Color(0xff232476),
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                OtpTextField(
+                  numberOfFields: 4,
+                  fieldWidth: 60,
+                  enabledBorderColor: Colors.grey.shade400,
+                  borderColor: Color(0xFF512DA8),
+                  //set to true to show as box or false to show as dash
+                  showFieldAsBox: true,
+                  //runs when a code is typed in
+                  focusedBorderColor: Color(0xFF512DA8),
+                  onCodeChanged: (String code) {
+                    //handle validation or checks here
+                  },
+                  //runs when every textfield is filled
+                  onSubmit: (String verificationCode) {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text("Verification Code"),
+                            content: Text('Code entered is $verificationCode'),
+                          );
+                        });
+                  }, // end onSubmit
                 ),
               ],
             )
